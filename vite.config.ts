0896563fe5c457e638dce2a57b4029cd544360fd
msvclimber <msvclimber@yandex.ru>
2025-01-03 22:45:09 +0400

    eeerthj

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
-})
+  base: "/",
+  plugins: [react()],
+  // server: {
+  //   host: true,
+  //   port: 8000, // This is the port which we will use in docker
+  //   // Thanks @sergiomoura for the window fix
+  //   // add the next lines if you're using windows and hot reload doesn't work
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  // },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://localhost:8080",
+  },
+});
