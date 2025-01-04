b7003b337248636757c5f15fb146ff2322dd78b2
msvclimber <msvclimber@yandex.ru>
2025-01-04 20:59:59 +0400

    add routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   // server: {
   //   host: true,
   //   port: 8000, // This is the port which we will use in docker
